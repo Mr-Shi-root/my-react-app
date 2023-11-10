65dfa62c8bd2bf4e0be90d82948cd976c18d8978
小宁 <shijiangning@weidian.com>
2023-11-10 18:13:46 +0800

    feat: init antd

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,12 @@
+import {Button} from 'antd'
+console.log(Button);
+
 function Login() {
     return (
-        <h1>登陆</h1>
+        <>
+            <h1>登陆</h1>
+            <Button>登陆按钮</Button>
+        </>
     )
 }
 
